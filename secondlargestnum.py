1ebbb9f007ca3b2db1a69c06846159be97210d6a
dhokanerahul13 <115435716+dhokanerahul13@users.noreply.github.com>
2022-10-11 09:33:17 -0700

    comment

---
+++ secondlargestnum.py
@@ -0,0 +1,23 @@
+
+def max1(list):
+    l=[]
+    max=list[0]
+    for i in list:
+        if i>max:
+            max=i
+            print(l.append(i))
+    return list
+list=[11,22,33,44,5533,5533,56]
+print(max1(list))
+
+
+
+def max1(list):
+    list.sort()
+    print(list)
+list=[11,22,33,44,5533,5533,56]
+print(max1(list))
+
+
+
+
